6e47ea4c6b029296a41d6122c7d7879c8961ca7d
Andrew Klenner <atklenner@gmail.com>
2023-04-11 10:13:28 -0700

    add game room from previous project

---
+++ server/src/rooms/TicTacToe.ts
@@ -0,0 +1,31 @@
+import { Room, Client } from "colyseus";
+import { Dispatcher } from "@colyseus/command";
+import TicTacToeState from "./schema/TicTacToeState";
+import { Message } from "../../../types/messages";
+import PlayerSelectionCommand from "../commands/PlayerSelectionCommand";
+import { GameState } from "../../../types/gameState";
+
+export default class TicTacToe extends Room<TicTacToeState> {
+  private dispatcher = new Dispatcher(this);
+
+  onCreate() {
+    this.maxClients = 2;
+    this.setState(new TicTacToeState());
+
+    this.onMessage(Message.PlayerSelection, (client, message) => {
+      this.dispatcher.dispatch(new PlayerSelectionCommand(), {
+        client,
+        index: message.index,
+      });
+    });
+  }
+
+  onJoin(client: Client) {
+    const idx = this.clients.findIndex((c) => c.sessionId === client.sessionId);
+    client.send(Message.PlayerIndex, { playerIndex: idx });
+
+    if (this.clients.length >= 2) {
+      this.state.gameState = GameState.Playing;
+    }
+  }
+}
