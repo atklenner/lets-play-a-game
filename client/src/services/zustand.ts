922c01cef12b2bb95a321e9612d91ad810fddf30
Andrew Klenner <atklenner@gmail.com>
2023-04-13 12:16:06 -0700

    setWinner now sets gameOver to true

--- client/src/services/zustand.ts
+++ client/src/services/zustand.ts
@@ -4,18 +4,23 @@ interface ReactStore {
   login: boolean;
   pickGame: boolean;
   gameOver: boolean;
+  winner: boolean;
   toggleLogin: () => void;
   togglePickGame: () => void;
   toggleGameOver: () => void;
+  setWinner: (winner: boolean) => void;
 }
 
 const reactStore = create<ReactStore>()((set) => ({
   login: true,
   pickGame: false,
   gameOver: false,
+  winner: false,
   toggleLogin: () => set((state) => ({ login: !state.login })),
   togglePickGame: () => set((state) => ({ pickGame: !state.pickGame })),
   toggleGameOver: () => set((state) => ({ gameOver: !state.gameOver })),
+  setWinner: (win: boolean) =>
+    set((_state) => ({ winner: win, gameOver: true })),
 }));
 
 export default reactStore;
